2287203c2ae756dd493309e1933d2651863dc3f1
Yuki <yk-nagai@kddilabs.jp>
2017-07-09 11:45:43 +0900

    test for facebook

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ http.createServer((req, res) => {
 
 console.log('Server running at http://localhost:1337/');
 
+var apiai = require('apiai');
+
+var app = apiai("507fa7521a514e4098d4e309f5f42502");
+
+var request = app.textRequest('Hello!!!!!', {
+    //sessionId: '<unique session id>'
+});
+
+request.on('response', function(response) {
+    console.log(response);
+});
+
+request.on('error', function(error) {
+    console.log(error);
+});
+
+request.end();
+
